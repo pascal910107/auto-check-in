70db8135aa03694a11b25edd5293b015158528bf
pascal910107 <95022717+pascal910107@users.noreply.github.com>
2022-11-28 05:08:08 +0800

    new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/*.js", "./public/index.html"],
+  content: ["./src/*.js", "./public/index.html", "./src/**/*.vue"],
   theme: {
     extend: {
       colors: {
@@ -7,6 +7,10 @@ module.exports = {
         b: "#2e8c8a",
         c: "#6aa287",
         d: "#e7ebf1",
+        e: "#3da06b",
+        f: "#5ebe80",
+        g: "#3a3e67",
+        h: "#3d5170",
       },
     },
   },
